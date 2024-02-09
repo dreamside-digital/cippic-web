f287e55cd28539f08b23342135414f201869b811
Sharon <sharon.peishan.kennedy@gmail.com>
2024-02-09 10:30:31 -0500

    smaller image on issue page header

--- components/sections/FancyHeader.js
+++ components/sections/FancyHeader.js
@@ -7,31 +7,34 @@ export default function FancyHeader({ before_title, title, subtitle, byline, ico
       <section className="section-md bg-two">
         <div className="container position-relative">
           <div className="row">
-            <div className={`${image ? "col-12 col-md-8" : "col-12"}`}>
-                {tags && <div className="tags">
-                    <i className="icon-folder" />
-                    {tags}
-                </div>}
-                <div className="title-with-icon">
-                  <h1 className="m-0 underline">{title}</h1>
+            <div className="issue-header col-12">
+              {
+                image && 
+                <div className="header-image">
+                  <div className="image-container highlight-border">
+                    <Image 
+                      src={`${process.env.NEXT_PUBLIC_STRAPI_DOMAIN}${image.formats.small.url}`} 
+                      width={image.formats.small.width} 
+                      height={image.formats.small.height} 
+                      alt={image.alternativeText} 
+                    />
+                  </div>
                 </div>
-                {byline && <p>{byline}</p>}
-                {subtitle && <p className="text-lg mb-4">{subtitle}</p>}
-                {button && <div className="mr_top_40"><ButtonLink href={button.button_link} target="_blank">{button.button_text}</ButtonLink></div>}
-                {children}
-            </div>
-            {
-              image && 
-              <div className="col-12 col-md-4 mb-3 header-image">
-                <Image 
-                  className="highlight-border img-fit" 
-                  src={`${process.env.NEXT_PUBLIC_STRAPI_DOMAIN}${image.formats.small.url}`} 
-                  width={image.formats.small.width} 
-                  height={image.formats.small.height} 
-                  alt={image.alternativeText} 
-                />
+              }
+              <div className={`header-content`}>
+                  {tags && <div className="tags">
+                      <i className="icon-folder" />
+                      {tags}
+                  </div>}
+                  <div className="title-with-icon">
+                    <h1 className="m-0 underline">{title}</h1>
+                  </div>
+                  {byline && <p>{byline}</p>}
+                  {subtitle && <p className="text-lg mb-4">{subtitle}</p>}
+                  {button && <div className="mr_top_40"><ButtonLink href={button.button_link} target="_blank">{button.button_text}</ButtonLink></div>}
+                  {children}
               </div>
-            }
+            </div>
           </div>
         </div>
       </section>
